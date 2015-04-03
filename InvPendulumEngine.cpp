09b3de944d8b6811cfbc234fd5bd2c7a19982afe
Kai Huang <khuang29@illinois.edu>
2015-04-03 10:05:13 -0500

    Added InvPendulumEngine class skeleton code.

---
+++ InvPendulumEngine.cpp
@@ -0,0 +1,27 @@
+#include "InvPendulumEngine.h"
+
+InvPendulumEngine::InvPendulumEngine()
+{
+    //ctor
+}
+
+InvPendulumEngine::~InvPendulumEngine()
+{
+    //dtor
+}
+
+InvPendulumEngine::InvPendulumEngine(const InvPendulumEngine& other)
+{
+    //copy ctor
+}
+
+InvPendulumEngine& InvPendulumEngine::operator=(const InvPendulumEngine& rhs)
+{
+    if (this == &rhs) return *this; // handle self assignment
+    //assignment operator
+    return *this;
+}
+
+InvPendulumEngine::step() {
+
+}
---
+++ InvPendulumEngine.h
@@ -0,0 +1,45 @@
+#ifndef INVPENDULUMENGINE_H
+#define INVPENDULUMENGINE_H
+
+
+class InvPendulumEngine
+{
+    public:
+        InvPendulumEngine();
+        ~InvPendulumEngine();
+        InvPendulumEngine(const InvPendulumEngine& other);
+        InvPendulumEngine& operator=(const InvPendulumEngine& other);
+
+        void step();
+
+        double (*controller) (double cart_pos, double pen_angle);
+        // getters and setters
+        double Get_sumulation_freq() { return simulation_freq; }
+        void Set_sumulation_freq(double val) { simulation_freq = val; }
+        double Get_pen_len() { return pen_len; }
+        void Set_pen_len(double val) { pen_len = val; }
+        double Get_pen_mass() { return pen_mass; }
+        void Set_pen_mass(double val) { pen_mass = val; }
+        double Get_pen_angle() { return pen_angle; }
+        void Set_pen_angle(double val) { pen_angle = val; }
+        double Get_cart_mass() { return cart_mass; }
+        void Set_cart_mass(double val) { cart_mass = val; }
+        double Get_cart_pos() { return cart_pos; }
+        void Set_cart_pos(double val) { cart_pos = val; }
+        double Get_gravity() { return gravity; }
+        void Set_gravity(double val) { gravity = val; }
+        double Get_cart_v() { return cart_v; }
+        void Set_cart_v(double val) { cart_v = val; }
+
+    private:
+        int simulation_freq;
+        double pen_len;
+        double pen_mass;
+        double pen_angle;
+        double cart_mass;
+        double cart_pos;
+        double gravity;
+        double cart_v;
+};
+
+#endif // INVPENDULUMENGINE_H
