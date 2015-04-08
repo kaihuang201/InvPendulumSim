384c77ecb04e277229ea9e563432239aa43793dd
eckhard3 <eckhard3@illinois.edu>
2015-04-08 16:03:19 -0500

    Initial pass at physics
    Right now, Euler integration only.  Placeholder for RK4, just need to
    add the code to the function.  Euler is simpler, so we'll start with
    that.

--- InvPendulumEngine.cpp
+++ InvPendulumEngine.cpp
@@ -1,8 +1,23 @@
 #include "InvPendulumEngine.h"
+#include <cmath>
 
 InvPendulumEngine::InvPendulumEngine()
 {
     //ctor
+    this->pen_len = 1.0;
+    this->pen_mass = 1.0;
+    this->pen_angle = 0.0;
+	this->pen_angular_velocity = 0.0;
+    this->cart_mass = 10.0;
+    this->cart_pos = 0.0;
+    this->cart_v = 0.0;
+
+	this->cur_force = 0.0;
+	this->prev_force = 0.0;
+
+	this->gravity = 9.81;
+
+	this->step_im = EULER;
 }
 
 InvPendulumEngine::~InvPendulumEngine()
@@ -22,6 +37,61 @@ InvPendulumEngine& InvPendulumEngine::operator=(const InvPendulumEngine& rhs)
     return *this;
 }
 
-InvPendulumEngine::step() {
+void InvPendulumEngine::step()
+{
+	//Stuff...
 
+	//Update the physics
+	//Call the correct function based on the chosen integration method
+	switch(this->step_im)
+	{
+	case EULER:
+		this->UpdatePhysicsEuler();
+		break;
+	case RK4:
+		this->UpdatePhysicsRK4();
+		break;
+	default:
+		//Default to Euler
+		this->UpdatePhysicsEuler();
+		break;
+	}
+
+	//Stuff...
+}
+
+void InvPendulumEngine::UpdatePhysicsEuler()
+{
+	//Rename variables to match pdf
+	double m=this->pen_mass;
+	double M=this->cart_mass;
+	double L=this->pen_len;
+	double g=this->gravity;
+	double h=simulation_freq;
+	double x1=this->cart_pos, x2=this->cart_v, x3=this->pen_angle, x4=this->pen_angular_vel;
+	double u = this->force;
+
+	//Create temporary variable to hold new values without overwriting old ones
+	double new_cart_pos=0.0, new_cart_v=0.0, new_pen_angle=0.0, new_pen_omega=0.0;
+
+	// Euler integration
+	new_cart_pos = x1 + dt*x2;
+	new_cart_v = x2 + h*( (m*g*sin(x3)*cos(x3) - m*L*sqr(x4)*sin(x3)-u) / (m*sqr(cos(x3)) - (M+m)) );
+	new_pen_angle = x3 + dt*x4;
+	new_pen_omega = this->x4 + h*( (-1.0*(M+m)*g*sin(x3) + m*L*sqr(x4)*sin(x3)*cos(x3) + u*cos(x3)) / (m*L*sqr(cos(x3))-(M+m)*L) );
+
+	//Update the simulation's state variables with the new, calculated values
+	this->pen_angle = new_pen_angle;
+	this->pen_angular_vel = new_pen_omega;
+	this->cart_pos = new_cart_pos;
+	this->cart_v = new_cart_v;
+}
+
+void InvPendulumEngine::UpdatePhysicsRK4()
+{
+	//TODO
+
+	//Right now, since RK4 isn't implemented yet, just pass into Euler update
+	//Ideally, this function won't be called until RK4 is implemented
+	this->UpdatePhysicsEuler();
 }
--- InvPendulumEngine.h
+++ InvPendulumEngine.h
@@ -13,14 +13,16 @@ class InvPendulumEngine
         void step();
 
         // getters and setters
-        double Get_sumulation_freq() { return simulation_freq; }
-        void Set_sumulation_freq(double val) { simulation_freq = val; }
+        double Get_simulation_freq() { return simulation_freq; }
+        void Set_simulation_freq(double val) { simulation_freq = val; }
         double Get_pen_len() { return pen_len; }
         void Set_pen_len(double val) { pen_len = val; }
         double Get_pen_mass() { return pen_mass; }
         void Set_pen_mass(double val) { pen_mass = val; }
         double Get_pen_angle() { return pen_angle; }
         void Set_pen_angle(double val) { pen_angle = val; }
+		double Get_pen_angular_vel() { return pen_angular_vel; }
+		void Set_pen_angular_vel(double val) { pen_angular_vel = val; }
         double Get_cart_mass() { return cart_mass; }
         void Set_cart_mass(double val) { cart_mass = val; }
         double Get_cart_pos() { return cart_pos; }
@@ -31,6 +33,14 @@ class InvPendulumEngine
         void Set_cart_v(double val) { cart_v = val; }
         void Set_controller_fun( double (*controller) (double, double) ) { controller = val; }
 
+		enum IntegrationMethod {
+			EULER,
+			RK4,
+		};
+
+		IntegrationMethod GetIntegrationMethod() { return step_im; }
+		void SetIntegrationMethod(IntegrationMethod im) { step_im = im; }
+
     private:
         int simulation_freq;
         
@@ -40,13 +50,24 @@ class InvPendulumEngine
         double pen_len;
         double pen_mass;
         double pen_angle;
+		double pen_angular_vel;
         double cart_mass;
         double cart_pos;
+		double cart_v;
+
+		double force;
+
         double gravity;
-        double cart_v;
         
         // controller function pointer, returns servo position signal.
         double (*controller) (double cart_pos, double pen_angle);
+
+		IntegrationMethod step_im;
+
+		double sqr(double val) { return val*val; }
+
+		void UpdatePhysicsEuler();
+		void UpdatePhysicsRK4();
 };
 
 #endif // INVPENDULUMENGINE_H
